148ed6be78cef64162a5d62b97919f4facbc8ede
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-05-07 22:59:01 +0530

    Cart Page Added

--- regstuff/src/App.js
+++ regstuff/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import {Login} from './Admin/Login';
 import Register from './Admin/Register';
-import CartPage from './Pages/CartPage';
+
 
 function App() {
   return (
     <div className="App">
-      <CartPage/>
+     
     </div>
   );
 }
