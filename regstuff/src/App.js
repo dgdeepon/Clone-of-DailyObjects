bd85a59542c35b7a0cb8fea484e6e10f5d7c4844
dgdeepon <deeponghosh890@gmail.com>
2023-05-07 16:53:17 +0530

    account page responsive

--- regstuff/src/App.js
+++ regstuff/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import {Login} from './Admin/Login';
 import Register from './Admin/Register';
+import MainRoute from './routes/MainRoute';
 
 
 function App() {
   return (
     <div className="App">
-
+      <MainRoute/>
     </div>
   );
 }
